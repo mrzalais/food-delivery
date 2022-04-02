37d0a21f75f6f7eb00f84ec2b154a77614735137
mrzalais <lejnieksedijs@gmail.com>
2022-04-02 12:55:01 +0300

    Improve Map Parser - Extract values as constants, add more map values, add more test cases for parser

--- src/Models/MapParser.php
+++ src/Models/MapParser.php
@@ -4,38 +4,69 @@ namespace App\Models;
 
 class MapParser
 {
+    public const TYPE_BUILDING = 'building';
+    public const TYPE_WATER = 'water';
+    public const TYPE_LAND = 'land';
+    public const TYPE_ROAD = 'road';
+    public const TYPE_USER = 'user';
+
+    public const TYPE_UNKNOWN = 'unknown';
+
     public function parse(string $map): array
     {
-        $lines = explode("\n", $map);
+        $lines = explode("|", $map);
         $parsedMap = [];
 
         foreach ($lines as $i => $line) {
+            $line = trim($line);
+            $currentLine = [];
             $max = strlen($line);
             for($j = 0; $j < $max; $j++) {
                 switch ($line[$j]) {
-                    case 'W':
-                        $parsedMap[] = [
+                    case 'B':
+                        $currentLine[] = [
                             'x' => $j,
                             'y' => $i,
-                            'type' => 'building'
+                            'type' => self::TYPE_BUILDING,
+                        ];
+                        break;
+                    case 'W':
+                        $currentLine[] = [
+                            'x' => $j,
+                            'y' => $i,
+                            'type' => self::TYPE_WATER,
+                        ];
+                        break;
+                    case 'L':
+                        $currentLine[] = [
+                            'x' => $j,
+                            'y' => $i,
+                            'type' => self::TYPE_LAND,
                         ];
                         break;
                     case '_':
-                        $parsedMap[] = [
+                        $currentLine[] = [
                             'x' => $j,
                             'y' => $i,
-                            'type' => 'road'
+                            'type' => self::TYPE_ROAD,
                         ];
                         break;
                     case 'X':
-                        $parsedMap[] = [
+                        $currentLine[] = [
                             'x' => $j,
                             'y' => $i,
-                            'type' => 'user'
+                            'type' => self::TYPE_USER,
                         ];
-                    break;
+                        break;
+                    default:
+                        $currentLine[] = [
+                            'x' => $j,
+                            'y' => $i,
+                            'type' => self::TYPE_UNKNOWN,
+                        ];
                 }
             }
+            $parsedMap [] = $currentLine;
         }
 
         return $parsedMap;
