9f4352542e9667c6a64425dab5c984af5723874e
mrzalais <lejnieksedijs@gmail.com>
2022-04-03 11:15:48 +0300

    Add Faker for data faking and later for factories, Add more data to User class

--- src/Models/User.php
+++ src/Models/User.php
@@ -2,4 +2,30 @@
 
 namespace App\Models;
 
-class User {}
+class User {
+    private string $email;
+    private string $mobileNumber;
+    private string $address;
+
+    public function __construct(string $email, string $mobileNumber, string $address)
+    {
+        $this->email = $email;
+        $this->mobileNumber = $mobileNumber;
+        $this->address = $address;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getMobileNumber(): string
+    {
+        return $this->mobileNumber;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+}
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -4,19 +4,28 @@ declare(strict_types=1);
 
 namespace Tests\Leaderboard;
 
-use App\Models\User;
 use App\Models\Order;
+use App\Models\Recipient;
 use PHPUnit\Framework\TestCase;
+use Faker;
 
 class UserTest extends TestCase
 {
     public function testItCanHaveAOrder(): void
     {
-        $user = new User;
-        $order = new Order(0, 0, [1, 1]);
-        $user->setOrder($order);
+        $faker = Faker\Factory::create();
 
-        $this->assertEquals($user->getOrder(), $order);
+        //TODO create factory
+        $recipient = new Recipient(
+            $faker->email(),
+            $faker->phoneNumber(),
+            $faker->address(),
+        );
+
+        $order = new Order(0, 0, [1, 1]);
+        $recipient->setOrder($order);
+
+        $this->assertEquals($recipient->getOrder(), $order);
     }
 
     //TODO it can have order history
